b9c0956b08970ddbcbcb8f0ff6e2341b320a2918
Diego <diegovalrus@gmail.com>
2025-06-03 16:43:08 -0400

    init

---
+++ src/content/mosaico/bento-images.js
@@ -0,0 +1,8 @@
+export const bento = [
+    '/public/bento/Burger.jpg',
+    '/public/bento/buses.jpg',
+    '/public/bento/delivery.jpg',
+    '/public/bento/gym.jpg',
+    '/public/bento/pizza.jpg',
+    '/public/bento/taxi.jpg'
+]
---
+++ tailwind.config.mjs
@@ -0,0 +1,8 @@
+import animated from '@midudev/tailwind-animations';
+
+export default {
+  content: [
+    "./src/**/*.{astro,js,ts,jsx,tsx,html}",
+  ],
+  plugins: [animated],
+};
